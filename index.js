e721e7342471e4cfe1fe7876a6d47d6d228b45d5
cancio <filipe.cancio@outlook.com>
2023-05-23 23:47:21 -0300

    criando logica

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-
 const express = require('express');
+const database = require("./database.json");
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const data = {
-    "message": "Hello World"  
+  "message": "Hello World"
 }
 
 app.get('/', (req, res) => {
-  res.status(200).send(data);
+  res.status(200).send(getTransactions());
 });
+
+const getTransactions = () => database
+  .sort((item) => item.timestamp)
+  .groupBy('bank');
+
+Array.prototype.groupBy = function (prop) {
+  return this.reduce((acc, item) => {
+    if (!acc[item[prop]]) acc[item[prop]] = []
+    acc[item[prop]].push(item)
+    return acc
+  }, {});
+}
 
 app.listen(port);
 console.log(`Aplicação executando no endereco http://localhost:${port}`);
